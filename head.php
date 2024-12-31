7b08988f5994e758899c2a41414380ac5c3b0fb1
Darshan Mehta <darshanm28403@gmail.com>
2024-12-31 15:36:57 +0530

    Second Commit

--- head.php
+++ head.php
@@ -1,3 +1,4 @@
+<html>
 <head>
 	<title>Kiddos - Free Bootstrap 4 Template by Colorlib</title>
 	<meta charset="utf-8">
@@ -22,3 +23,51 @@
 	<script src="https://kit.fontawesome.com/79cdea9a89.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="css/style.css">
 </head>
+<body>
+	<div class="py-2 bg-primary">
+		<div class="container">
+			<div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
+				<div class="col-lg-12 d-block">
+					<div class="row d-flex">
+						<div class="col-md-5 pr-4 d-flex topper align-items-center">
+							<div class="icon bg-fifth mr-2 d-flex justify-content-center align-items-center"><span
+									class="icon-map"></span></div>
+							<span class="text">198 West 21th Street, Suite 721 New York NY 10016</span>
+						</div>
+						<div class="col-md pr-4 d-flex topper align-items-center">
+							<div class="icon bg-secondary mr-2 d-flex justify-content-center align-items-center"><span
+									class="icon-paper-plane"></span></div>
+							<span class="text">stemaplus@sproutingskills.com</span>
+						</div>
+						<div class="col-md pr-4 d-flex topper align-items-center">
+							<div class="icon bg-tertiary mr-2 d-flex justify-content-center align-items-center"><span
+									class="icon-phone2"></span></div>
+							<span class="text">+91 99250 10033</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
+		<div class="container d-flex align-items-center">
+			<a class="navbar-brand" href="index.html"><img src="images/logo.png" alt=""></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+				aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="oi oi-menu"></span> Menu
+			</button>
+			<div class="collapse navbar-collapse" id="ftco-nav">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active"><a href="index.html" class="nav-link pl-0">Home</a></li>
+					<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+					<li class="nav-item"><a href="stem.html" class="nav-link">STEM Club</a></li>
+					<li class="nav-item"><a href="courses.html" class="nav-link">Courses</a></li>
+					<li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li>
+					<li class="nav-item"><a href="faqs.html" class="nav-link">FAQs</a></li>
+					<li class="nav-item"><a href="gallery.html" class="nav-link">Gallery</a></li>
+					<li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!-- END nav -->
